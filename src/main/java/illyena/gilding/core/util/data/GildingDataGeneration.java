66e38767204c802631f3feec8b1f2b736a77c940
Illyena <nel.huff@gmail.com>
2023-04-27 10:16:22 -0700

    converted modId and structure to Avengers

--- src/main/java/illyena/gilding/core/util/data/GildingDataGeneration.java
+++ src/main/java/illyena/gilding/core/util/data/GildingDataGeneration.java
@@ -1,20 +1,20 @@
 package illyena.gilding.core.util.data;
 
-import illyena.gilding.birthday.util.data.*;
+import illyena.gilding.avengers.util.data.*;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class GildingDataGeneration implements DataGeneratorEntrypoint {
     @Override
     public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-        fabricDataGenerator.addProvider(BirthdayModelProvider::new);
+        fabricDataGenerator.addProvider(AvengersModelProvider::new);
 
-        fabricDataGenerator.addProvider(BirthdayLootTableProvider::new);
+        fabricDataGenerator.addProvider(AvengersLootTableProvider::new);
 
         fabricDataGenerator.addProvider(GildingBlockTagGenerator::new);
-        fabricDataGenerator.addProvider(BirthdayBlockTagGenerator::new);
+        fabricDataGenerator.addProvider(AvengersBlockTagGenerator::new);
         fabricDataGenerator.addProvider(GildingItemTagGenerator::new);
-        fabricDataGenerator.addProvider(BirthdayItemTagGenerator::new);
-        fabricDataGenerator.addProvider(BirthdayPaintingTagGenerator::new);
+        fabricDataGenerator.addProvider(AvengersItemTagGenerator::new);
+        fabricDataGenerator.addProvider(AvengersPaintingTagGenerator::new);
     }
 }
